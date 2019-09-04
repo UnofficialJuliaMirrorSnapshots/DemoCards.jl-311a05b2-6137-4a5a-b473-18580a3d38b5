74526e4e46f7afd64cb2ea9d694d81f9c49adf8f
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-04 10:36:49 +0000

    Snapshot of branch master taken on 2019-09-04T06:36:48.286-04:00 from "https://github.com/UnofficialJuliaMirror/DemoCards.jl-311a05b2-6137-4a5a-b473-18580a3d38b5"

---
+++ src/types/card.jl
@@ -0,0 +1,68 @@
+abstract type AbstractDemoCard end
+
+"""
+    democard(path::String)::T
+
+Constructs a concrete AbstractDemoCard instance.
+
+The return type `T` is determined by the extension of the path
+to your demofile. Currently supported types are:
+
+* [`MarkdownDemoCard`](@ref)
+"""
+function democard(path::String)::AbstractDemoCard
+    validate_file(path)
+    _, ext = splitext(path)
+    if ext in markdown_exts
+        return MarkdownDemoCard(path)
+    else
+        throw(ArgumentError("unrecognized democard format $(path)"))
+    end
+end
+
+basename(x::AbstractDemoCard) = basename(x.path)
+
+function get_default_id(card::AbstractDemoCard)
+    name_without_ext = splitext(basename(card))[1]
+    # default documenter id has -1 suffix
+    replace(name_without_ext, ' ' => '-') * "-1"
+end
+
+function validate_id(id::AbstractString, card::AbstractDemoCard)
+    if occursin(' ', id)
+        throw(ArgumentError("invalid id in $(card.path), it should not contain spaces."))
+    end
+end
+
+function load_config(card::T, key) where T <: AbstractDemoCard
+    config = parse(card)
+
+    if key == "cover"
+        root = dirname(card.path)
+        haskey(config, key) || return nothing
+
+        cover_path = joinpath(root, config[key])
+        isfile(cover_path) || throw(ArgumentError("$(cover_path) isn't a valid image file for cover."))
+        return cover_path
+    elseif key == "id"
+        haskey(config, key) || return get_default_id(card)
+
+        id = config[key]
+        validate_id(id, card)
+        return id
+    elseif key == "title"
+        return get(config, key) do
+            name_without_ext = splitext(basename(card))[1]
+            strip(replace(uppercasefirst(name_without_ext), "_" => " "))
+        end
+    elseif key == "description"
+        return get(config, key, card.title)
+    else
+        throw(ArgumentError("Unrecognized key $(key) for $(T)"))
+    end
+end
+
+
+### load concrete implementations
+
+include("markdown.jl")
---
+++ src/types/markdown.jl
@@ -0,0 +1,109 @@
+# generally, Documenter.jl assumes markdown files ends with `.md`
+const markdown_exts = [".md",]
+
+"""
+    struct MarkdownDemoCard <: AbstractDemoCard
+    MarkdownDemoCard(path::String)
+
+Constructs a markdown-format demo card from existing markdown file `path`.
+
+# Fields
+
+Besides `path`, this struct has some other fields:
+
+* `path`: path to the source markdown file
+* `cover`: path to the cover image
+* `id`: cross-reference id
+* `title`: one-line description of the demo card
+* `description`: multi-line description of the demo card
+
+# Configuration
+
+You can pass additional information by adding a YAML front matter to the markdown file.
+Supported items are:
+
+* `cover`: relative path to the cover image. If not specified, it will use the first available image link, or all-white image if there's no image links.
+* `description`: a multi-line description to this file, will be displayed when the demo card is hovered. By default it uses `title`.
+* `id`: specify the `id` tag for cross-references. By default it's infered from the filename, e.g., `simple_demo` from `simple demo.md`.
+* `title`: one-line description to this file, will be displayed under the cover image. By default, it's the name of the file (without extension).
+
+An example of the front matter:
+
+```text
+---
+title: passing extra information
+cover: cover.png
+id: non_ambiguious_id
+description: this demo shows how you can pass extra demo information to DemoCards package.
+---
+```
+
+See also: [`DemoSection`](@ref DemoCards.DemoSection), [`DemoPage`](@ref DemoCards.DemoPage)
+"""
+struct MarkdownDemoCard <: AbstractDemoCard
+    path::String
+    cover::Union{String, Nothing}
+    id::String
+    title::String
+    description::String
+end
+
+function MarkdownDemoCard(path::String)::MarkdownDemoCard
+    # first consturct an incomplete democard, and then load the config
+    card = MarkdownDemoCard(path, "", "", "", "")
+
+    cover = load_config(card, "cover")
+    id    = load_config(card, "id")
+    title = load_config(card, "title")
+    card = MarkdownDemoCard(path, cover, id, title, "")
+
+    # default description requires a title
+    description = load_config(card, "description")
+    MarkdownDemoCard(path, cover, id, title, description)
+end
+
+"""
+    parse(card::MarkdownDemoCard)
+
+parse out configuration of markdown files and return it as a `Dict`.
+
+Possible configuration resources are:
+
+* YAML front matter
+* image links
+
+!!! note
+
+    Users of this function need to use `haskey` to check if keys are existed.
+    They also need to validate the values.
+"""
+function parse(card::MarkdownDemoCard)
+    contents = split(read(card.path, String), "---\n")
+
+    if length(contents) == 1
+        config = Dict()
+        body = join(contents)
+    else
+        config = YAML.load(strip(contents[2]))
+        body = join(contents[3:end])
+    end
+
+    if !haskey(config, "cover")
+        # set the first valid image path as cover
+        # TODO: only markdown syntax is supported now
+        image_paths = map(eachmatch(regex_md_img, body)) do m
+            m.captures[1]
+        end
+        filter!(x->isfile(dirname(card.path), x), image_paths)
+        if !isempty(image_paths)
+            config["cover"] = first(image_paths)
+        end
+    end
+
+    if haskey(config, "cover")
+        config["cover"] = replace(config["cover"],
+                                  r"[/\\]" => Base.Filesystem.path_separator) # windows compatibility
+    end
+
+    return config
+end
